ab455af04e311f9d9e376a8be44d6d51bb2a9170
Isis Amaya <icamayaa@eafit.edu.com>
2023-10-29 17:15:06 -0500

    mejoras en la tabla y correcion de error en imagen

--- module1/views.py
+++ module1/views.py
@@ -31,7 +31,7 @@ def secante(request):
         
         
         secante_model = secanteModel(func = request.POST["func"], x0 = request.POST["x0"], x1=request.POST["x1"], Tol = request.POST["Tol"], niter = request.POST["niter"],error = request.POST["error"])
-       
+
         context = {
             'secante_model': secante_model,
         }        
@@ -51,25 +51,23 @@ def biseccion(request):
 
         eng = matlab.engine.start_matlab()
 
-        resultado = eng.Biseccion(str(request.POST["func"]) ,float(request.POST["xi"]), float(request.POST["xs"]), float(request.POST["tol"]), float(request.POST["iteraciones"]))
-        
-        a = matlab.double(resultado)
+        result = eng.Biseccion(str(request.POST["func"]) ,float(request.POST["xi"]), float(request.POST["xs"]), float(request.POST["tol"]), float(request.POST["iteraciones"]))
         
         
-        biseccion_model = BiseccionModel(func = request.POST["func"], xi = request.POST["xi"], xs=request.POST["xs"], tol = request.POST["tol"], iteraciones = request.POST["iteraciones"])
+        df = pd.read_csv('tables/tabla_biseccion.csv')
+        df = df.astype(str)
+        data = df.to_dict(orient='records')
         
         
-        
-        tabla_dict = [{'Iteration': row[0], 'a': row[1], 'xi': row[2], 'b': row[3], 'f': row[4], 'Error': row[5]} for row in a]
-        
-        print(tabla_dict)
+        biseccion_model = BiseccionModel(func = request.POST["func"], xi = request.POST["xi"], xs=request.POST["xs"], tol = request.POST["tol"], iteraciones = request.POST["iteraciones"], resultado = result)
         
         context = {
-        'tabla': tabla_dict,
         'biseccion_model': biseccion_model,
+        'data': data,
         }
         
         biseccion_model.save()
+        
         
         
         return render(request, "biseccion.html", context)
